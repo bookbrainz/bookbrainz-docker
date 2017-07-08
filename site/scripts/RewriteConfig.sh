c4cc0dcddee7f0860627ea4932842dcd4489e8e8
Ben Ockmore <ben.sput@gmail.com>
2017-07-08 22:48:29 +0100

    feat: add dedicated docker site config file

--- site/scripts/RewriteConfig.sh
+++ site/scripts/RewriteConfig.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
-cp development.json.example config.json
-
-# Check if ENABLE_INFLUX is set (regardless of value)
-if [[ ${ENABLE_INFLUX+x} ]]
-then
-  sed -i "s|\"site\": {|\"influx\": {},\n\t\"site\": {|" config.json
-fi
 sed -i "s/\"clientID\": \"\"/\"clientID\": \"$MB_OAUTH_CLIENT_ID\"/" config.json
 sed -i "s/\"clientSecret\": \"\"/\"clientSecret\": \"$MB_OAUTH_CLIENT_SECRET\"/" config.json
 sed -i "s/\"callbackURL\": \"\"/\"callbackURL\": \"$MB_OAUTH_CALLBACK_URL\"/" config.json
 sed -i "s/\"secret\": \"\"/\"secret\": \"$SESSION_SECRET\"/" config.json
-sed -i "s/\"host\": \"localhost:9200\"/\"host\": \"es:9200\"/" config.json
-sed -i "s/\"host\": \"localhost\"/\"host\": \"redis\"/" config.json
-sed -i "s/\"host\": \"127.0.0.1\"/\"host\": \"db\"/" config.json
-sed -i "s/\"user\": \"bookbrainz\"/\"user\": \"postgres\"/" config.json
-sed -i "s/\"password\": \"bookbrainz\"/\"password\": \"\"/" config.json
 
 cp config.json development.json
 cp config.json production.json
